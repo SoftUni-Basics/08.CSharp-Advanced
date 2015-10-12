5b9a4849f1c4bfcba91937617756de90dd2ed9fa
GogoK <georgiakanchev@gmail.com>
2015-10-13 01:19:02 +0300

    difference between dates

---
+++ 01.DifferenceBetweenDates/DifferenceBetweenDates.cs
@@ -0,0 +1,23 @@
+﻿/*  Write a program that enters two dates in format dd.MM.yyyy and returns the number of days between them.
+*/
+
+using System;
+using System.Globalization;
+
+class DifferenceBetweenDates
+{
+    static void Main()
+    {
+        string format = "d.MM.yyyy";
+        DateTime[] dates = new DateTime[2];
+
+        for (int i = 0; i < dates.Length; i++)
+        {
+            string input = Console.ReadLine();
+            dates[i] = DateTime.ParseExact(input, format, CultureInfo.InvariantCulture);
+        }
+
+        int daysDifference = (dates[1] - dates[0]).Days;
+        Console.WriteLine("Days difference:  {0}", daysDifference);
+    }
+}
