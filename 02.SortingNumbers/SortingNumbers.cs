6f9fedeceb9f68913905db0c46d50bbf3aa2c615
GogoK <georgiakanchev@gmail.com>
2015-10-13 01:32:43 +0300

    sorting numbers

---
+++ 02.SortingNumbers/SortingNumbers.cs
@@ -0,0 +1,29 @@
+﻿/*  Write a program that reads a number 'n' and a sequence of 'n' integers, sorts them and prints them.
+*/
+
+using System;
+
+class SortingNumbers
+{
+    static void Main()
+    {
+        Console.Write("Enter count of numbers:  ");
+        int count = int.Parse(Console.ReadLine());
+        int[] numbers = new int[count];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write("No{0}:  ", i + 1);
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine();
+
+        Array.Sort(numbers);
+        Console.WriteLine("Sorted numbers:");
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+        Console.WriteLine();
+    }
+}
