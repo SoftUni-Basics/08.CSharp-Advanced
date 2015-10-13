04c4ab64a082081cffa028df97a7064cac25c789
GogoK <georgiakanchev@gmail.com>
2015-10-13 20:44:53 +0300

    longest word in a text

---
+++ 08.LongestWordInAText/LongestWordInAText.cs
@@ -0,0 +1,24 @@
+﻿/*  Write a program to find the longest word in a text.
+*/
+
+using System;
+using System.Text.RegularExpressions;
+
+class LongestWordInAText
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+        string[] words = Regex.Split(text, @"\W+");
+
+        string longest = string.Empty;
+        foreach (var word in words)
+        {
+            if (word.Length > longest.Length)
+            {
+                longest = word;
+            }
+        }
+        Console.WriteLine(longest);
+    }
+}
